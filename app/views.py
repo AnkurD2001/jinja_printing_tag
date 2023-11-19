149798e5a1da48eeccc33501e23522d311f0ef03
AnkurD2001 <ankur98amg@gmail.com>
2023-11-19 10:03:18 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def data_render(request):
+    data='This is our assumption'
+    d={'assumption':data}
+    return render(request,'data_render.html',context=d)
+
+def login(request):
+    d={'username':'Ankur'}
+    return render(request,'login.html',context=d)
